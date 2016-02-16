2504ada3322eab46fc50c20cc1670a0ee2603662
Ryan Edwards <reedwards@getty.edu>
2016-02-16 09:55:41 -0800

    Update analysis2bibs.sh

--- analysis2bibs.sh
+++ analysis2bibs.sh
@@ -30,7 +30,14 @@ cat /exlibris/primo/newTitles/mmsids_wolabel.txt|
   while read ids 
       do echo "curl -G 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/$ids?expand=p_avail&apikey={your_api_key}' -o /exlibris/primo/newTitles/records/bibrec_$ids.xml" >> /exlibris/primo/newTitles/bibs.sh
       done
- 
+
+# Initially, when we created analysis2bibs.sh, we encountered a bug in the Alma Analytics API/Oracle BI which prevented us 
+# from retrieving the list of Alma MMSIDs when using the Alma Analytics API (reference case #00103091).  So, we decided to 
+# retrieve the Alma MMSIDs from Box.com instead. However, we added commented out code in analysis2bibs.sh, thinking that 
+# ExLibris might fix this bug in the future.  ExLibris did indeed fix the bug in the Alma February 2016 Release, but we decided 
+# to keep using Box.com because the maximum number of MMSIDs which can be retrieved per the Alma Analytics API call is 1000 
+# and we didn't want to be involved with complicated multiple API calls, resumption tokens, loops, python scripts, etc...
+
 #use booksReceived.xml and report_js.xsl to isolate mmsids
 # /exlibris/primo/p4_1/product/local/java/bin/java -jar /exlibris/primo/newTitles/saxon9he.jar -s:/exlibris/primo/newTitles/booksReceived.xml -xsl:/exlibris/primo/newTitles/report_js.xsl -o:/exlibris/primo/newTitles/mmsids.txt
  
